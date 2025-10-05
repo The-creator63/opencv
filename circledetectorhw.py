b38e2ac5047474175932dc96560c44f5cef69100
The-creator63 <eshantaqi@icloud.com>
2025-10-05 17:10:54 +0700

    Add files via upload

--- circledetectorhw.py
+++ circledetectorhw.py
@@ -11,4 +11,23 @@ if detector is not None:
         x,y,r = i[0],i[1],i[2]
         cv2.circle(img,(x,y),r,(0,0,0),3)
         cv2.imshow("Image circle",img)
+img = cv2.imread("/Users/muhammadtaqiriaz/Documents/opencv/istockphoto-1368965646-612x612.jpg",1)
+blobdetect = cv2.SimpleBlobDetector_Params()
+blobdetect.filterByArea = True
+blobdetect.minArea = 800
+blobdetect.filterByCircularity = False
+blobdetect.minCircularity = 0.25
+blobdetect.filterByConvexity = False
+blobdetect.minConvexity = 0.1
+detector1 = cv2.SimpleBlobDetector_create(blobdetect)
+point = detector1.detect(img)
+elements = len(point)
+font = cv2.FONT_HERSHEY_COMPLEX
+position = (0,75)
+size = (1.5)
+text = (str(elements))
+imgtext = cv2.putText(img,text,position,font,size,(0,0,0),3,cv2.LINE_AA)
+cv2.imshow("blobs",imgtext)
+img_blobs = cv2.drawKeypoints(img,point,img,(0,0,0),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow("Blob detectiom",img_blobs)
 cv2.waitKey(0)
