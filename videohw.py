6f0bba935dd460da08f5c6e234099c6c3dc7d8eb
The-creator63 <eshantaqi@icloud.com>
2025-10-26 17:20:03 +0700

    Add files via upload

--- videohw.py
+++ videohw.py
@@ -22,4 +22,17 @@ for n in list:
         img_store2 = Image.open(os.path.join(path,n))
         img_resize = img_store2.resize((mwidth,mheight),Image.Resampling.LANCZOS)
         img_resize.save(n,"JPEG",quality = 95)
+video_name = "videohw.avi"
+os.chdir("/Users/muhammadtaqiriaz/Documents/opencv/images_videohw")
+images =  []
+for l in list:
+    if l.endswith((".jpg",".jpeg")):
+        images.append(l)
+frame = cv2.imread(os.path.join(".",images[0]))
+print(frame.shape)
+width2,height2,colour = frame.shape
+video = cv2.VideoWriter(video_name,cv2.VideoWriter_fourcc(*"XVID"),1,(width2,height2))
+for g in images:
+    video.write(cv2.imread(os.path.join(".",g)))
+video.release()
 cv2.waitKey(0)
