7114177c5f18ca1035e9e68d7711f6822aff168c
The-creator63 <eshantaqi@icloud.com>
2025-11-09 18:07:21 +0700

    Add files via upload

---
+++ face.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+path = ("/Users/muhammadtaqiriaz/Documents/opencv/Face_detection/haarcascade_frontalface_default.xml")
+image_set = ("/Users/muhammadtaqiriaz/Documents/opencv/Face_detection/image_set")
+folder = ("/Users/muhammadtaqiriaz/Documents/opencv/Face_detection/image_set/Eshan")
+path_save = os.path.join(image_set,folder)
+width = 130
+height = 100
+face_detection = cv2.CascadeClassifier(path)
+webcam = cv2.VideoCapture(0)
+for i in range(1,30):
+    value,image = webcam.read()
+    cv2.imshow("image",image)
+    img_grey = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    face = face_detection.detectMultiScale(img_grey,1.3,4)
+    for x,y,w,h in face:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+        faces =  img_grey[y:y+h,x:x+w]
+    
+    cv2.waitKey(0)
